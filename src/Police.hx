4b6db8d5364e6c03f81154fd55459fd29bc0d24d
bartv <bart.veldstra@engine-software.nl>
2015-08-23 11:11:22 +0200

    Use proper graphic for cops

--- src/Police.hx
+++ src/Police.hx
@@ -1,6 +1,7 @@
 package ;
 
 import com.haxepunk.Entity;
+import com.haxepunk.graphics.Spritemap;
 import com.haxepunk.HXP;
 import openfl.geom.Point;
 import com.haxepunk.utils.Input;
@@ -25,16 +26,20 @@ class Police extends Entity
     var decisionTimer:Float;
     var startDistance:Float;
     var hud:HUD;
+    var sprite:Spritemap;
 
     public function new(x:Float, y:Float, hud:HUD)
     {
         super(x, y);
         this.hud = hud;
-        var img = ImageFactory.createRect(5, 10, 0x0033FF);
-        img.originY = img.height - img.width / 2;
-        graphic = img;
+        sprite = ImageFactory.createSpriteSheet("graphics/police.png", 7);
+        sprite.add("walk", [0, 1], 8, true);
+        sprite.add("aim", [2]);
+        sprite.add("fire", [3]);
+        sprite.originY = sprite.height - sprite.width / 2;
+        graphic = sprite;
         direction = new Point();
-        ImageFactory.setEntityHitboxTo(this, img);
+        ImageFactory.setEntityHitboxTo(this, sprite);
         decisionTimer = 0;
         enterScreen();
     }
@@ -46,6 +51,7 @@ class Police extends Entity
         direction.normalize(WalkSpeed);
         state = AIState.EnteringScreen;
         startDistance = HXP.random * HXP.halfWidth + HXP.halfWidth / 2;
+        sprite.play("walk");
     }
 
     function dashRandom()
@@ -55,12 +61,14 @@ class Police extends Entity
         direction.normalize(WalkSpeed);
         state = AIState.Running;
         decisionTimer = HXP.random * (MaxRunTime - MinRunTime) + MinRunTime;
+        sprite.play("walk");
     }
 
     function shoot()
     {
         state = AIState.Shooting;
         decisionTimer = ShootDelay;
+        sprite.play("aim");
     }
 
     override public function update():Void
@@ -95,5 +103,7 @@ class Police extends Entity
         }
 
         layer = ZOrder.layerByY(y);
+
+        sprite.flipped = direction.x < 0;
     }
 }
